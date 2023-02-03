0069fadaf087fa8e8d07e15d1dab87f419510b53
yong.teng <tengong@pengbei.tech>
2023-02-03 22:47:23 +0800

    修复所有权限标识符错误的 BUG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,13 @@
 # 更新日志
 
+## [v1.1.3](https://github.com/buession/buession-shiro/releases/tag/1.1.3)（2023-02-03）
 
-## v1.1.2
+### 🐞 BUG 修复
+
+- 修复所有权限标识符错误的 BUG
+
+
+## [v1.1.2](https://github.com/buession/buession-shiro/releases/tag/1.1.1)（2023-02-01）
 
 ### 🐞 BUG 修复
 
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,21 +1,17 @@
-export const hasAny = (data: string[] | null | undefined, value: any): boolean => {
-	if (data === null || data === undefined) {
-		return false;
-	}
+import { Un } from "@/core/types";
 
-	return data.indexOf(value) >= 0;
+export const hasAny = (data: string[] | Un, value: any): boolean => {
+	return Array.isArray(data) && data.indexOf(value) >= 0;
 }
 
-export const hasAll = (data: string[] | null | undefined, values: any[] | null | undefined): boolean => {
-	if ((data === null || data === undefined) || (values === null || values === undefined || values.length === 0)) {
-		return false;
-	}
-
-	for (let i = 0; i < values.length; i++) {
-		if (data.indexOf(values[i]) < 0) {
-			return false;
+export const hasAll = (data: string[] | Un, values: any[] | Un): boolean => {
+	if (Array.isArray(data) && (Array.isArray(values) && values.length > 0)) {
+		for (let i = 0; i < values.length; i++) {
+			if (data.indexOf(values[i]) >= 0) {
+				return true;
+			}
 		}
 	}
 
-	return true;
+	return false;
 }
